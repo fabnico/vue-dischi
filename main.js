fbcf2a248d70fbc56f681a80b2b4706c2f8a37ac
Fabrizio <whoet.wo@gmail.com>
2020-12-01 17:05:38 +0100

    Implementazione bonus

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const app = new Vue({
    el: '#root',
    data:{
-      cds:[]
+      cds: null,
+      filteredCds: null,
+      genre: ''
+   },
+   methods:{
+      filterCd: function(){
+      this.filteredCds = this.cds.filter(e => e.genre == this.genre);
+      }
    },
    mounted: function(){
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-      .then(result => this.cds = result.data.response)
+      .then(result => this.cds = result.data.response);
    }
 });
